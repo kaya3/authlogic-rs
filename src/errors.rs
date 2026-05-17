6b43856032685d54719cb1e1fe7195104f79fdd6
Andrew Kay <warfreak2@gmail.com>
2026-05-17 11:14:04 +0100

    Derive thiserror::Error

--- src/errors.rs
+++ src/errors.rs
@@ -1,29 +1,34 @@
 use actix_web::http::StatusCode;
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     /// Indicates that the user is authenticated but cannot continue until they
     /// verify their email address. This can happen if a user registers a new
     /// account with a password, then uses their password to log in before
     /// completing the email verification challenge.
+    #[error("Email address is not verified")]
     EmailNotVerified,
 
     /// Indicates that the user tried to complete an email challenge, but the
     /// code in their challenge link is not correct. The challenge might
     /// already have been completed, or expired, or it never existed.
+    #[error("Incorrect challenge code")]
     IncorrectChallengeCode,
 
     /// Indicates that the user is not authenticated in a context where they
     /// need to be.
+    #[error("User is not authenticated")]
     NotAuthenticated,
 
     /// Indicates that the user is authenticated but cannot continue until they
     /// choose a new password. This can happen if a new user is created with a
     /// temporary password, or the user completed a password reset challenge.
+    #[error("User is required to change their password")]
     RequirePasswordChange,
 
     /// Indicates that the user is authenticated but cannot continue, because
     /// their account has been suspended.
+    #[error("User is suspended")]
     UserIsSuspended,
 
     /// Internal error which occurs when an authenticated user attempts to
@@ -33,19 +38,23 @@ pub enum Error {
     /// This either indicates a logic error in your `AppDb` implementation, or
     /// a race condition in which the user is deleted after the session cookie
     /// is verified but before the reauthentication is checked.
+    #[error("User data query failed for id = {user_id}")]
     UserDataQueryFailed {user_id: i64},
 
     /// Internal error which occurs when serializing or deserializing challenge
     /// data.
-    Serde(serde_json::Error),
+    #[error("Corrupted challenge data")]
+    Serde(#[source] serde_json::Error),
     
     /// Internal error which occurs when hashing a new password.
-    NewPasswordHash(password_hash::Error),
+    #[error("Failed to generate new password hash")]
+    NewPasswordHash(#[source] password_hash::Error),
     
     /// Internal error which occurs when verifying a password. This could
     /// indicate, for example, that a hash stored in the database is in the
     /// wrong format, or uses an unsupported algorithm.
-    StoredPasswordHash(password_hash::phc::Error),
+    #[error("Failed to parse stored password hash")]
+    StoredPasswordHash(#[source] password_hash::phc::Error),
 }
 
 impl Error {
